16ae869d4df605488f0e3da2bfe83e9dd1ca3a4d
Ferronn <ferronn@ferronn.dev>
2021-12-25 13:35:36 -0600

    store wowless logs

--- query.sh
+++ query.sh
@@ -7,4 +7,4 @@ mkdir -p extracts
 unzip -qq -o -d extracts "mount/extracts/$version.zip"
 lua tools/gextract.lua "mount/gscrapes/$version.lua" > "extracts/$version/Interface/GlobalEnvironment.lua"
 ln -sf "$version" "extracts/$product"
-env HOME=/root /usr/local/bin/lua wowless.lua "$loglevel" "$product" "$gameversion"
+env HOME=/root /usr/local/bin/lua wowless.lua "$loglevel" "$product" "$gameversion" > "mount/logs/$product-$(date +%s).txt"
